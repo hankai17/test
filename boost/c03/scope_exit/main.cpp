c555196e3d151c308be3c126ce9118c1f3d56239
hankai17 <867614535@qq.com>
2019-08-10 10:07:43 +0800

    Update main.cpp

--- boost/c03/scope_exit/main.cpp
+++ boost/c03/scope_exit/main.cpp
@@ -3,10 +3,12 @@
 #include <cstdio>
 #include <cassert>
 
+//当一个程序因为返回或异常离开当前作用域时 finally或scope块中的代码将会执行
+
 int main() {
     std::FILE* f = std::fopen("example_file.txt", "w");
     assert(f);
-    BOOST_SCOPE_EXIT(f) {
+    BOOST_SCOPE_EXIT(f) { //无论怎样都会执行这个函数
     // Whatever happened in scope, this code will be executed
     // and file will be correctly closed.
         std::fclose(f);
@@ -22,7 +24,7 @@ public:
 
     void theres_more_example_func() {
         std::FILE* f = 0;
-        BOOST_SCOPE_EXIT(f, this_) { // Capture object `this_`.
+        BOOST_SCOPE_EXIT(f, this_) { // Capture object `this_`. //传递多个值
             this_->close(f);
         } BOOST_SCOPE_EXIT_END
     }
