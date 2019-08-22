0c7df4217ddeeb55b1e4d5153150bab9ddf45a8c
root <root@vultr.guest>
2019-08-22 02:21:54 +0000

    rm melon

--- melon/inc/global.hh
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include"namespaces.hh"
-#include"socket.hh"
-#include"event.hh"
-
-extern void func_test(int fd, boost::any args);
-extern int arecvtcp(string& data, size_t len, Socket* sock, bool incomplete_oky);
-extern int asendtcp(const string& data, Socket* sock);
-//extern bool operator<(const RWEvent& a, const RWEvent& b);
--- melon/src/event.cc
+++
@@ -1,9 +0,0 @@
-#include "event.hh"
-
-bool RWEvent::operator<(const RWEvent& b) const {
-  if(this->what < b.what) return true;
-  if(this->what > b.what) return false;
-  if(this->who < b.who) return true;
-  return false;
-}
-
